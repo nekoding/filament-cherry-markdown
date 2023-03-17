2a56d62a73b0aa581d10073b9576f063c9b58dbf
enggar tivandi <enggar.appkey@gmail.com>
2023-03-17 09:33:54 +0800

    wip: handle debounce data input

--- resources/views/cherryMarkdownField.blade.php
+++ resources/views/cherryMarkdownField.blade.php
@@ -1,13 +1,33 @@
-<x-forms::field-wrapper :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()"
-    :hint-action="$getHintAction()" :hint-color="$getHintColor()" :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
-    <div x-data="" x-init="editor = new Cherry({
-        id: 'cherry-editor',
-        locale: 'en_US',
-        editor: {
-            height: '500px',
-        },
-        forceAppend: true
-    })" wire:ignore="true">
+<x-forms::field-wrapper
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :hint-action="$getHintAction()"
+    :hint-color="$getHintColor()"
+    :hint-icon="$getHintIcon()"
+    :required="$isRequired()"
+    :state-path="$getStatePath()"
+>
+    <div
+        x-data=""
+        x-init="cherryEditor = new Cherry({
+            id: 'x',
+            value: state,
+            locale: 'en_US',
+            editor: {
+                height: '500px',
+            },
+            forceAppend: true,
+            callback: {}
+        });
+        
+        cherryEditor.onChange(debounce((e) => {
+            state = String(e.markdown)
+        }))"
+        wire:ignore
+    >
         <div id="cherry-editor"></div>
     </div>
 </x-forms::field-wrapper>
