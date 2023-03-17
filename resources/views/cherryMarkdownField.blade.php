1e587cea813575f68163981e1f69bbaade9a4e76
enggar tivandi <enggar.appkey@gmail.com>
2023-03-17 10:18:20 +0800

    wip: add config editor

--- resources/views/cherryMarkdownField.blade.php
+++ resources/views/cherryMarkdownField.blade.php
@@ -10,17 +10,60 @@
     :required="$isRequired()"
     :state-path="$getStatePath()"
 >
+
+    <script>
+        function debounce(func, timeout = 300) {
+            let timer;
+            return (...args) => {
+                clearTimeout(timer);
+                timer = setTimeout(() => {
+                    func.apply(this, args);
+                }, timeout);
+            };
+        }
+
+        let cherryEditorConfiguration = @json(config('filament-cherry-markdown.editor_config'));
+    </script>
+
     <div
-        x-data=""
+        x-data="{ state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $getStatePath() . '\')') }} }"
         x-init="cherryEditor = new Cherry({
-            id: 'x',
+            id: 'cherry-editor',
             value: state,
-            locale: 'en_US',
             editor: {
-                height: '500px',
+                height: cherryEditorConfiguration.height || '100%',
+                defaultModel: cherryEditorConfiguration.default_model || 'edit&preview',
             },
-            forceAppend: true,
-            callback: {}
+            toolbars: {
+                toolbar: [
+                    @if ($hasToolbarButton('bold')) 'bold', @endif
+                    @if ($hasToolbarButton('italic')) 'italic', @endif
+                    @if ($hasToolbarButton('strikethrough')) 'strikethrough', @endif '|',
+                    @if ($hasToolbarButton('color')) 'color', @endif
+                    @if ($hasToolbarButton('header')) 'header', @endif '|',
+                    @if ($hasToolbarButton('list')) 'list', @endif {
+                        insert: [
+                            @if ($hasToolbarButton('image')) 'image', @endif
+                            @if ($hasToolbarButton('audio')) 'audio', @endif
+                            @if ($hasToolbarButton('video')) 'video', @endif
+                            @if ($hasToolbarButton('link')) 'link', @endif
+                            @if ($hasToolbarButton('hr')) 'hr', @endif
+                            @if ($hasToolbarButton('br')) 'br', @endif
+                            @if ($hasToolbarButton('code')) 'code', @endif
+                            @if ($hasToolbarButton('formula')) 'formula', @endif
+                            @if ($hasToolbarButton('toc')) 'toc', @endif
+                            @if ($hasToolbarButton('table')) 'table', @endif
+                            @if ($hasToolbarButton('line-table')) 'line-table', @endif
+                            @if ($hasToolbarButton('bar-table')) 'bar-table', @endif
+                            @if ($hasToolbarButton('pdf')) 'pdf', @endif
+                            @if ($hasToolbarButton('word')) 'word', @endif
+                        ]
+                    },
+                    @if ($hasToolbarButton('graph')) 'graph', @endif
+                    @if ($hasToolbarButton('settings')) 'settings', @endif
+                ]
+            },
+            locale: cherryEditorConfiguration.locale || 'zh_CN',
         });
         
         cherryEditor.onChange(debounce((e) => {
--- src/FilamentCherryMarkdownServiceProvider.php
+++ src/FilamentCherryMarkdownServiceProvider.php
@@ -23,5 +23,8 @@ class FilamentCherryMarkdownServiceProvider extends PluginServiceProvider
             'https://cdn.jsdelivr.net/npm/cherry-markdown/dist/cherry-markdown.min.css',
             'spatie-markdown-editor' => __DIR__ . '/../resources/css/plugin.css',
         ]);
+
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'filament-cherry-markdown');
     }
 }
